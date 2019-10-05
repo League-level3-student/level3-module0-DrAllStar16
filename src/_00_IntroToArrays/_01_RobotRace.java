561a1c7a10d5420f89efe6efed7d674b63229113
League Student <league@iMac-27.attlocal.net>
2019-10-04 17:00:44 -0700

    Worked on Robot Race

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -11,13 +11,13 @@ public class _01_RobotRace {
 	public static void main(String[] args) {
 
 		// 2. create an array of 5 robots.
-		Robot[] bot = new Robot[5];
+		Robot[] bot = new Robot[6];
 
 		// 3. use a for loop to initialize the robots.
 		for (int i = 0; i < bot.length; i++) {
 			bot[i] = new Robot();
-			bot[i].setX(200 * i + 50);
-			bot[i].setY(550);
+			bot[i].setX(50);
+			bot[i].setY(300);
 			bot[i].setSpeed(10);
 		}
 		// 4. make each robot start at the bottom of the screen, side by side, facing up
@@ -25,12 +25,14 @@ public class _01_RobotRace {
 		// 5. use another for loop to iterate through the array and make each robot move
 		// a random amount less than 50.
 		Boolean rRacing = true;
+		int turns = 0;
 		
 		while (rRacing) {
 			for (int i = 0; i < bot.length; i++) {
-				if (bot[i].getY() < 50) {
+				if (bot[i].getY() == 300 && bot[i].getX()==50 && turns != 0) {
 					rRacing = false;
-					
+					bot[i].turn(720);
+					JOptionPane.showMessageDialog(null, "Robot "+(i+1)+" "+ "won!");
 					
 				}
 
@@ -38,8 +40,13 @@ public class _01_RobotRace {
 
 			Random r = new Random();
 			for (int l = 0; l < bot.length; l++) {
-				bot[l].move(r.nextInt(50));
+				for (int i = 0; i < r.nextInt(5)+1; i++) {
+					bot[l].move(50);
+					bot[l].turn(10);
+				}
+				
 			}
+			turns++;
 		} // 6. use a while loop to repeat step 5 until a robot has reached the top of the
 			// screen.
 
